d336b08312ff842ad42d087e370c0af3d0094d95
Frank <f.neumann.berlin@googlemail.com>
2023-02-28 09:20:51 +0100

    fertige Version

--- app/src/main/java/com/example/tictactoecompose/MainActivity.kt
+++ app/src/main/java/com/example/tictactoecompose/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
@@ -22,8 +24,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
+import com.example.tictactoecompose.TicTacToeViewModel.allFields
 import com.example.tictactoecompose.TicTacToeViewModel.currentUser
 import com.example.tictactoecompose.TicTacToeViewModel.currentUserText
+import com.example.tictactoecompose.TicTacToeViewModel.isPlaying
+import com.example.tictactoecompose.TicTacToeViewModel.isWon
+import com.example.tictactoecompose.TicTacToeViewModel.startColor
 import com.example.tictactoecompose.ui.theme.TicTacToeComposeTheme
 
 var screenWidth: Float = 0f
@@ -32,6 +38,10 @@ var screenHeigt: Float = 0f
 object TicTacToeViewModel : ViewModel() {
     var currentUser by mutableStateOf("X")
     var currentUserText = "X"
+    var isPlaying = true
+    var isWon = false
+    val startColor = Color.White
+    var allFields by mutableStateOf(Array(3) { Array(3) { "" } })
 }
 
 class MainActivity : ComponentActivity() {
@@ -49,11 +59,25 @@ class MainActivity : ComponentActivity() {
 
                         BoxWithConstraints(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .background(Color.Red, RectangleShape),
+                                .fillMaxSize(),
                             contentAlignment = Center
                         ) {
                             Spielfeld(rows = 3, cols = 3)
+                            FloatingActionButton(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .align(BottomEnd),
+                                onClick = {
+                                    isPlaying = false
+                                    isWon = false
+                                    currentUser = ""
+                                    currentUser = "X"
+                                    currentUserText = "X"
+                                    allFields = Array(3) { Array(3) { "" } }
+                                }
+                            ) {
+                                Text(text = "Reset")
+                            }
                         }
                     }
                 }
@@ -74,7 +98,10 @@ fun Status(name: String) {
             .height(36.dp)
     ) {
         Text(
-            text = stringResource(id = R.string.status_play, currentUser),
+            text = if (isWon)
+                stringResource(id = R.string.status_win, currentUser)
+            else
+                stringResource(id = R.string.status_play, currentUser),
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentWidth(CenterHorizontally)
@@ -85,7 +112,7 @@ fun Status(name: String) {
 }
 
 @Composable
-fun Feld(modifier: Modifier) {
+fun Feld(modifier: Modifier, row: Int, col: Int) {
     var bColor by remember {
         mutableStateOf(Color.White)
     }
@@ -93,13 +120,18 @@ fun Feld(modifier: Modifier) {
         .fillMaxSize(1f)
 
         .clickable {
-            bColor = feldOnClick(bColor)
+            bColor = feldOnClick(bColor, row, col)
         }
-        .background(bColor),
+        .background(
+            if (isPlaying) bColor else {
+                bColor = startColor
+                startColor
+            }
+        ),
         contentAlignment = Center)
     {
         Text(
-            text = if (bColor == Color.White) "" else currentUserText,
+            text = if (bColor == Color.White) "" else allFields[row][col],
 //            modifier = Modifier
 //                .background(bColor),
             fontSize = 36.sp,
@@ -108,8 +140,9 @@ fun Feld(modifier: Modifier) {
     }
 }
 
-fun feldOnClick(bColor: Color): Color {
+fun feldOnClick(bColor: Color, row: Int, col: Int): Color {
     var retColor = bColor
+    if (!isPlaying) isPlaying = !isPlaying
     if (retColor == Color.White) {
 
         if (currentUser == "X") {
@@ -121,8 +154,31 @@ fun feldOnClick(bColor: Color): Color {
             currentUser = "X"
             currentUserText = "O"
         }
+        allFields[row][col] = currentUserText
+        if (checkWin()) {
+            isWon = true
+            currentUser = currentUserText
+        }
     }
     return retColor
+}
+
+fun checkWin(): Boolean {
+    val rows =
+        (allFields[0][0] == allFields[0][1] && allFields[0][0] == allFields[0][2] && allFields[0][0] != "") ||
+                (allFields[1][0] == allFields[1][1] && allFields[1][0] == allFields[1][2] && allFields[1][0] != "") ||
+                (allFields[2][0] == allFields[2][1] && allFields[2][0] == allFields[2][2] && allFields[2][0] != "")
+
+    val cols =
+        (allFields[0][0] == allFields[1][0] && allFields[0][0] == allFields[2][0] && allFields[0][0] != "") ||
+                (allFields[0][1] == allFields[1][1] && allFields[0][1] == allFields[2][1] && allFields[0][1] != "") ||
+                (allFields[0][2] == allFields[1][2] && allFields[0][2] == allFields[2][2] && allFields[0][2] != "")
+
+    val diagonal =
+        (allFields[0][0] == allFields[1][1] && allFields[0][0] == allFields[2][2] && allFields[0][0] != "") ||
+                (allFields[0][2] == allFields[1][1] && allFields[0][2] == allFields[2][0] && allFields[0][2] != "")
+
+    return rows || cols || diagonal
 }
 
 @Composable
@@ -137,18 +193,20 @@ fun Spielfeld(rows: Int, cols: Int) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = CenterHorizontally
     ) {
-        repeat(rows) {
+        repeat(rows) { row ->
             Row(
                 modifier = Modifier
                     .fillMaxWidth(1f)
                     .weight(1f)
             ) {
-                repeat(cols) {
+                repeat(cols) { col ->
                     Feld(
                         Modifier
                             .fillMaxSize(1f)
                             .weight(1f)
-                            .border(1.dp, Color.Black)
+                            .border(1.dp, Color.Black),
+                        row,
+                        col
                     )
                 }
             }
